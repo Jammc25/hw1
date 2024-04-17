daaf0b91b558a526605cad1eaf17fd86e05794c7
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-04-17 23:54:28 +0000

    First pass through of making the domain relationships, creating the tables and inserting some of the data

--- kmdb.sql
+++ kmdb.sql
@@ -112,14 +112,80 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS actors;
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    MPAA_rating TEXT,
+    studios_id INTEGER
+);
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    movie_id INTEGER,
+    actor_id INTEGER
+);
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
-
+INSERT INTO movies (
+    "title",
+    "year_released",
+    "MPAA_rating",
+    "studios_id"
+)
+VALUES (
+    "Batman Begins",
+    "2005",
+    "PG-13",
+    "1"
+);
+INSERT INTO movies (
+    "title",
+    "year_released",
+    "MPAA_rating",
+    "studios_id"
+)
+VALUES (
+    "The Dark Knight",
+    "2008",
+    "PG-13",
+    "1"
+);
+INSERT INTO movies (
+    "title",
+    "year_released",
+    "MPAA_rating",
+    "studios_id"
+)
+VALUES (
+    "The Dark Knight Rises",
+    "2012",
+    "PG-13",
+    "1"
+);
+INSERT INTO studios (
+    "name"
+)
+VALUES (
+    "Warner Bros."
+);
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
