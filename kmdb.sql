82180635857713b5a40cfa0dd9998c8ba021d94a
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-04-18 05:28:08 +0000

    Finished inputting data for the tables

--- kmdb.sql
+++ kmdb.sql
@@ -133,8 +133,8 @@ CREATE TABLE studios (
 CREATE TABLE roles (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    movie_id INTEGER,
-    actor_id INTEGER
+    movies_id INTEGER,
+    actors_id INTEGER
 );
 CREATE TABLE actors (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -185,6 +185,248 @@ INSERT INTO studios (
 )
 VALUES (
     "Warner Bros."
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Christian Bale"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Michael Caine"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Liam Neeson"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Katie Holmes"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Gary Oldman"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Heath Ledger"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Aaron Eckhart"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Maggie Gyllenhaal"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Tom Hardy"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Joseph Gordon-Levitt"
+);
+INSERT INTO actors (
+    "name"
+)
+
+VALUES (
+    "Anne Hathaway"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Bruce Wayne",
+    "1",
+    "1"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Bruce Wayne",
+    "2",
+    "1"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Bruce Wayne",
+    "3",
+    "1"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Commissioner Gordon",
+    "1",
+    "5"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Commissioner Gordon",
+    "3",
+    "5"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Alfred",
+    "1",
+    "2"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Alfred",
+    "2",
+    "2"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Rachel Dawes",
+    "1",
+    "4"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Rachel Dawes",
+    "2",
+    "8"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Ra's Al Ghul",
+    "1",
+    "3"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Joker",
+    "2",
+    "6"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Harvey Dent",
+    "2",
+    "7"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Bane",
+    "3",
+    "9"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "John Blake",
+    "3",
+    "10"
+);
+INSERT INTO roles (
+    "name",
+    "movies_id",
+    "actors_id"
+)
+
+VALUES (
+    "Selina Kyle",
+    "3",
+    "11"
 );
 -- Prints a header for the movies output
 .print "Movies"
